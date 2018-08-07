ed1911f77acdf9efc2a4868ed51be2be75e63de7
daijun <904315004@qq.com>
2018-08-07 21:14:09 +0800

    代码提交2018.8.7

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 require('./index.css')
 const commin = require('./img/commin.png');
+const getSucces = require('./img/getSucces.png')
 
 
 // 检查手机号格式
@@ -7,7 +8,7 @@ var checkMobile = function checkMobile() {
     var sMobile = $('#test').val();
     if (!(/^1[3|4|5|8][0-9]\d{4,8}$/.test(sMobile))) {
         return false;
-    }else return true;
+    } else return true;
 }
 
 // 提交数据方法
@@ -17,12 +18,15 @@ var updata = function() {
         url: `/api/archives/network?phone=${phone}&type=2`,
         type: "post",
         dataType: 'json',
-        contentType:"application/json",
-        data: formdata,
+        contentType: "application/json",
         success: function(data) {
-            if(data.retCode === 200) {
+            console.log(data)
+            if (data.retCode === 200) {
+                // debugger;
+                getOk();
+                alert(data.message)
+            } else {
                 updateStyle();
-            }else{
                 alert(data.message);
             }
         }
@@ -30,20 +34,33 @@ var updata = function() {
 }
 
 // 修改样式状态
-var updateStyle = function(){
-    $("#appWexin").css('display', 'block');
-    $('.appInput').css("display", "none");
-    $('.appButton').css("display", "none");
+var changeStyle = function() {
+        $("#appWexin").css('display', 'block');
+        $('.appInput').css("display", "none");
+        $('.appButton').css("display", "none");
+    }
+    //已经注册过的用户
+var updateStyle = function() {
     $('#firstImg').attr("src", commin);
+    changeStyle()
+}
+var getOk = function() {
+    $('#firstImg').attr('src', getSucces)
+    changeStyle()
+
+
+
+
 }
 
 // 绑定点击事件
 $("#button").click(function() {
+    // debugger;
     const isPhone = checkMobile();
     // 手机号检查
-    if(isPhone){
+    if (isPhone) {
         updata()
-    }else{
+    } else {
         alert("请输入正确的手机号");
     }
 });
